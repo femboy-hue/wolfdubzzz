bff0cdd18006065bef8ec652db2f534e5315ef1d
femboy-hue <gamegamer1968@gmail.com>
2025-09-06 02:06:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 
-st.title("✨ Dubzzz_Valo's Streaming Hub 🎮")
+st.set_page_config(page_title="Dubzzz_Valo's Hub", layout="wide")
+
+st.title("✨ Dubzzz_Valo's Streaming Hub ✨")
 
 st.write("Welcome to Dubzzz_Valo's official streaming hub! Connect with me on my social platforms:")
 
 st.markdown("""
-- 📺 [YouTube](https://www.youtube.com/@Dubzzz_Valo)
+- 🎥 [YouTube](https://www.youtube.com/@Dubzzz_Valo)
 - 📸 [Instagram](https://www.instagram.com/dubzzz_valo/)
 - 🎵 [TikTok](https://www.tiktok.com/@dubzzz_valo)
 - 🎮 [Twitch](https://www.twitch.tv/dubzzz_valo)
 """)
 
-# Display images side by side
+# Display profile pic + background side by side
 col1, col2 = st.columns(2)
 
 with col1:
     st.image("profile_pic.png", caption="Dubzzz_Valo", width=250)
 
 with col2:
-    st.image("starry_sky.png", caption="https://fi.pinterest.com/pin/43699058854987207/", width=400)
+    st.image("starry_sky.png", caption="Starry Night Vibes 🌌", width=400)
