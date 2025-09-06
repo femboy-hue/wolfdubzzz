bb05554046e236a28152b32b7650e3b024c3d87c
femboy-hue <gamegamer1968@gmail.com>
2025-09-06 01:56:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import streamlit as st
 
-st.title("Dubzzz_Valo's Streaming Hub")
+st.title("✨ Dubzzz_Valo's Streaming Hub 🎮")
+
+st.write("Welcome to Dubzzz_Valo's official streaming hub! Connect with me on my social platforms:")
 
 st.markdown("""
-Welcome to Dubzzz_Valo's official streaming hub! Connect with me on my social platforms:
-
-- [YouTube](https://www.youtube.com/@Dubzzz_Valo)
-- [Instagram](https://www.instagram.com/dubzzz_valo/)
-- [TikTok](https://www.tiktok.com/@dubzzz_valo)
-- [Twitch](https://www.twitch.tv/dubzzz_valo)
+- 📺 [YouTube](https://www.youtube.com/@Dubzzz_Valo)
+- 📸 [Instagram](https://www.instagram.com/dubzzz_valo/)
+- 🎵 [TikTok](https://www.tiktok.com/@dubzzz_valo)
+- 🎮 [Twitch](https://www.twitch.tv/dubzzz_valo)
 """)
+
+# Display images side by side
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("profile_pic.png", caption="Dubzzz_Valo", width=250)
+
+with col2:
+    st.image("starry_sky.png", caption="Starry Night Sky", width=400)
